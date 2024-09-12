44450286617da0f4d98e9b92464c2eaeee0fd9e0
rachit74 <rachithooda09@gmail.com>
2024-09-12 20:47:47 +0530

    Add: db connection test

--- liteORM/tests.py
+++ liteORM/tests.py
@@ -1,6 +1,23 @@
 import unittest
+import sqlite3
+from orm import BaseModel
 
-from liteORM.orm import BaseModel
+class BaseModelTest(unittest.TestCase):
+    def setUp(self):
+        self.model = BaseModel('test_database.db')
+        # Create a test table
+        self.model.execute_query(
+            '''
+            CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name VARCHAR(20));
+            '''
+        )
+    
+    def close_connection(self):
+        self.model.connection.close()
 
-class BaseModelTest:
-    pass
+    def test_connection(self):
+        """
+        tests if the connection is being made successfully
+        """
+        self.assertIsInstance(self.model.connection, sqlite3.Connection)
+        self.assertIsInstance(self.model.cursor, sqlite3.Cursor)
