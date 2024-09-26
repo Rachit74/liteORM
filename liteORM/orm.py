c2808b7861dd802e3246391b6ba5095f0ad538d1
rachit74 <rachithooda09@gmail.com>
2024-09-26 20:07:43 +0530

    reworking

--- liteORM/orm.py
+++ liteORM/orm.py
@@ -1,5 +1,10 @@
 import sqlite3
 
+"""
+class BaseModel
+
+Base Model class starts a new connection with the database and provide basic queries to run
+"""
 class BaseModel:
     def __init__(self, db_name):
         """
@@ -8,16 +13,16 @@ class BaseModel:
         self.connection = sqlite3.connect(db_name)
         self.cursor = self.connection.cursor()
 
-    def execute_query(self, query, params=()):
-        """
-        function to execute to query with parms
-        """
-        self.cursor.execute(query, params)
-        self.connection.commit()
+    # def execute_query(self, query, params=()):
+    #     """
+    #     function to execute to query with parms
+    #     """
+    #     self.cursor.execute(query, params)
+    #     self.connection.commit()
 
-    def fetch_query(self, query, params=()):
-        """
-        function to execute to a query with params and fetch the records
-        """
-        self.cursor.execute(query, params)
-        return self.cursor.fetchall()
+    # def fetch_query(self, query, params=()):
+    #     """
+    #     function to execute to a query with params and fetch the records
+    #     """
+    #     self.cursor.execute(query, params)
+    #     return self.cursor.fetchall()
